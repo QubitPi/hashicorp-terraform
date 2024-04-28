adf1ffe658af31f9e78fbd0b64c7e53cb41847d8
Jack <jack20220723@gmail.com>
2024-04-28 22:23:12 +0800

    Use custom domain 'terraform.qubitpi.org'

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -52,5 +52,5 @@ REPO=$PRODUCT \
 HASHI_ENV=project-preview \
 LOCAL_CONTENT_DIR=$LOCAL_CONTENT_DIR \
 CURRENT_GIT_BRANCH=$CURRENT_GIT_BRANCH \
-PRODUCT_DOC_BASE_PATH=/hashicorp-terraform \
+PRODUCT_DOC_BASE_PATH="" \
 npm run build:deploy-preview
